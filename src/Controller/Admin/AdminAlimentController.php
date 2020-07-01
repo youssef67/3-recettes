b62793d35894bcd11fc07332808651fcd78a09e2
Youssef Moudni <youssefmoudni@macbook-air-de-youssef.home>
2020-07-01 13:29:27 +0200

    correctifs

--- src/Controller/Admin/AdminAlimentController.php
+++ src/Controller/Admin/AdminAlimentController.php
@@ -5,13 +5,15 @@ namespace App\Controller\Admin;
 use App\Entity\Aliment;
 use App\Form\AlimentType;
 use App\Repository\AlimentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminAlimentController extends AbstractController
 {
     /**
-     * @Route("/admin/aliment", name="adminAliments")
+     * @Route("/admin/aliment", name="admin_aliments")
      */
     public function aliments(AlimentRepository $repository)
     {
@@ -23,15 +25,46 @@ class AdminAlimentController extends AbstractController
     }
 
     /**
-     * @Route("/admin/aliment/{id}", name="adminAliments_Modification")
+     * @Route("/admin/aliment/{id}", name="admin_aliment_modification",  methods="GET|POST")
+     * @Route("/admin/aliment/ajout", name="admin_aliment_ajout")
      */
-    public function modification(Aliment $aliment)
+    public function ajoutEtModif(Aliment $aliment = null, Request $request, EntityManagerInterface $entityManager)
     {
+        if (!$aliment)
+        {
+            $aliment = new Aliment(); 
+        }
+
         $form = $this->createForm(AlimentType::class, $aliment);
 
-        return $this->render('admin/modificationAliment.html.twig', [
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $modif = $aliment->getId() !== null;
+            $entityManager->persist($aliment);
+            $entityManager->flush();
+            $this->addFlash("success", $modif ? "la modification a été effectuée" : "l'ajout a été effectuée");
+            return $this->redirectToRoute("admin_aliments");
+        }
+        
+        return $this->render('admin/modifEtAjoutAliment.html.twig', [
             'aliment' => $aliment,
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'isModification' => $aliment->getId() !== null
         ]);
+    }
+
+    /**
+     * @Route("/admin/aliment/{id}", name="admin_aliment_suppression", methods="delete")
+     */
+    public function suppression(Aliment $aliment, Request $request, EntityManagerInterface $entityManager)
+    { 
+        if ($this->isCsrfTokenValid("SUP" . $aliment->getId(), $request->get('_token')))
+        {
+            $entityManager->remove($aliment);
+            $entityManager->flush();
+            $this->addFlash("success", "la suppression a été effectuée");
+            return $this->redirectToRoute("admin_aliments");  
+        }
     }
 }
